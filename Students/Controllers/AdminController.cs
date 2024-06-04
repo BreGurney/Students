c9eb3111c5c85b09227002e690f6db3727c12f44
BreGurney <150399227+BreGurney@users.noreply.github.com>
2024-06-04 11:36:38 -1200

    Finished Entity Framework

---
+++ Students/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Students.Models;
+using Students.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Students.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (StudentsEntities db = new StudentsEntities())
+            {
+                //var signups = db.SignUps.Where(x => x.Removed == null).ToList();
+                var signups = (from c in db.SignUps
+                               where c.Removed == null
+                               select c).ToList();
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.Id = signup.Id;
+                    signupVm.StudentId = signup.StudentId;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+                return View(signupVms);
+            }
+        }
+        public ActionResult Remove(int Id)
+        {
+            using (StudentsEntities db = new StudentsEntities())
+            {
+                var signup = db.SignUps.Find(Id);
+                signup.Removed = DateTime.Now;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Students/Controllers/HomeController.cs
+++ Students/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Students.Models;
+using Students.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -12,6 +14,7 @@ namespace Students.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
             return View();
@@ -26,45 +29,22 @@ namespace Students.Controllers
             }
             else
             {
-                string connectionString = @"Data Source=LAPTOP-R6CK06S9;Initial Catalog=Students;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
-                string queryString = @"INSERT INTO Signups (StudentId, FirstName, LastName, EmailAddress) VALUES 
-                                        (@StudentId, @FirstName, @LastName, @EmailAddress)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (StudentsEntities db = new StudentsEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@StudentId", SqlDbType.VarChar);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.StudentId = studentId;
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
 
-                    command.Parameters["@StudentId"].Value = studentId;
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
-
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
                 }
+               
                 return View("Success");
             }
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
